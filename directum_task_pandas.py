a6c717f9e4d5f0b0f6e509587907bb25fd712975
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-07-04 10:40:35 +0400

    edit formatting

--- directum_task_pandas.py
+++ directum_task_pandas.py
@@ -1,12 +1,5 @@
-
-# coding: utf-8
-
-# In[61]:
-
-
 from pandas import read_csv as read
 path = "company.csv"
 data = read(path, delimiter=",")
 empl_not_recommend_to_dismissal = data[(data.Education==1)&(data.Skills>90)&(data.NotFired)]
 print(empl_not_recommend_to_dismissal)
-
